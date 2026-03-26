c019df561c7c218ece6ddc265c98e0bcbb8e65bd
Kriam <chakradhari246108@gmail.com>
2026-03-26 18:28:15 +0530

    task controllers written

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -6,11 +6,12 @@ import { APIerror } from "../utils/api-error.js"
 import { APIresponse } from "../utils/api-response.js"
 import mongoose, { Mongoose } from "mongoose"
 import { AvaliableUserRole, UserRoleEnum } from "../utils/constants.js"
-import { task, task } from "../models/task.model.js"
+import { task } from "../models/task.model.js"
 import { subtask } from "../models/subtask.model.js"
 import { url } from "inspector"
 import { MIMEType } from "util"
 import { appendFile } from "fs"
+import { pipeline } from "stream"
 
 
 // TASKS CRUD
@@ -50,11 +51,97 @@ const Createtask = async_handler(async (req, res) => {
 })
 
 const Gettask = async_handler(async (req, res) => {
+    const { projectId } = req.params
+    const project = await Project.findById(projectId)
+    if (!project) {
+        throw new APIerror(404, "Project not found")
+    }
+    const tasks = await task.find({
+        project: new mongoose.Types.ObjectId(projectId),
+    }).populate("assingedTo", "avatar username fullname")
 
+    return res
+        .status(200)
+        .json(
+            new APIresponse(201, tasks, "task Fetched sucessfully ")
+        )
 })
 
 const GettaskbyID = async_handler(async (req, res) => {
+    const { taskId } = req.params
+    const task = await task.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(taskId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localfeild: "assignedTo",
+                forigenfeild: "_id",
+                as: "assignedTo",
+                pipeline: [
+                    {
+                        _id: 1,
+                        username: 1,
+                        fullname: 1,
+                        avatar: 1,
 
+                    }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "subtasks",
+                localfeild: "_id",
+                forigenfeild: "task",
+                as: "subtasks",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localfeild: "CreatedBy",
+                            forigenfeild: "_id",
+                            as: "assignedTo",
+                            pipeline: [
+                                {
+                                    _id: 1,
+                                    username: 1,
+                                    fullname: 1,
+                                    avatar: 1,
+
+                                },
+                                {
+                                    $addfeilds: {
+                                        createdBy: {
+                                            $arrayElemAt: ["$createdBy", 0]
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addfeilds: {
+                $arrayElemAt: ["assignedTo", 0]
+            }
+        }
+
+    ])
+    if (!task) {
+        throw new APIerror(404, "task not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new APIresponse(200, task[0], "task fetched sucesfully")
+        )
 })
 
 
